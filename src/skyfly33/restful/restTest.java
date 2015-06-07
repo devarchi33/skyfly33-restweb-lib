9e26600a2729686d985f55bd5b4e915b3cd7a303
skyfly33 <imfly7@naver.com>
2015-06-07 21:19:57 +0900

    @restful web sample

---
+++ src/skyfly33/restful/restTest.java
@@ -0,0 +1,54 @@
+package skyfly33.restful;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import javax.servlet.http.HttpServletRequest;
+
+public class restTest {
+	public static void main(String[] args) 
+	{
+		//해외특허 > 서지 정보 > 서지상세정보 REST API 호출 URL, (URL은 홈페이지 DATA 상세정보에서 확인 가능.)
+		//[도메인 /openapi/rest/서비스명/오퍼레이션명]으로 구성.
+		String apiUrl = "http://plus.kipris.or.kr/openapi/rest/ForeignPatentBibliographicService/bibliographicInfo";
+
+		//해당 상품 승인 접근키. 회원가입 후 자동 발급되며 마이페이지에서 확인 가능
+		String accessKey = "Mad4tOdLVZ5oEvmCm9m6TF9Oyz/gmQCFEDy8/SHNVUo=";
+		//입력값 문헌번호 & 국가코드
+		String literatureNumber = "198100001145A1";
+		String countryCode = "WO";
+		
+		apiUrl += "?accessKey="+accessKey+"&literatureNumber="+literatureNumber+"&countryCode="+countryCode;
+		
+		try {
+			//REST API URL을 읽어들어 결과를 출력한다
+			URL url = new URL(apiUrl);
+			
+			InputStream is = url.openStream();
+			InputStreamReader isr = new InputStreamReader(is,"utf-8");	//인코딩
+			BufferedReader reader = new BufferedReader(isr);
+			StringBuffer buffer = new StringBuffer();
+			String line = null;
+			String tmpStr = null;
+			while((line = reader.readLine()) != null){
+				tmpStr = line.toString();
+				tmpStr = tmpStr.replaceAll(" ", "");
+				
+				if(!tmpStr.equals("")) buffer.append(line).append("\r\n");
+			}
+			reader.close();
+			
+			//REST API 결과 값.
+			String xmlResult = buffer.toString();
+			
+			System.out.println("ForeignPatentBibliographicService/bibliographicInfo RESULT => " + xmlResult);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
